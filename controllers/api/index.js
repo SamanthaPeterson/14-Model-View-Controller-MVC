ef141ebc4efd60e99b7e4370c8d2b051d8a1b834
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-09-12 17:58:07 -0600

    working with schema

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,6 @@
+//https://expressjs.com/en/guide/routing.html
+//Routing = how an application’s endpoints (URIs) respond to client requests
+//routing is defined using methods of the Express app
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
@@ -9,3 +12,14 @@ router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes)
 
 module.exports = router;
+
+//How to separate routes on Node.js and Express https://stackoverflow.com/questions/23923365/how-to-separate-routes-on-node-js-and-express-4
+//How to include route handlers in multiple files in Express https://stackoverflow.com/questions/6059246/how-to-include-route-handlers-in-multiple-files-in-express
+
+//https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/routes Routes and controllers
+// -"Routes"to forward the supported requests(and any information encoded in request URLs) to the appropriate controller functions.
+// -Controller functions to get the requested data from the models, create an HTML page displaying the data, and
+// return it to the user to view in the browser.
+// -Views(templates) used by the controllers to render the data.
+
+//https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Introduction
