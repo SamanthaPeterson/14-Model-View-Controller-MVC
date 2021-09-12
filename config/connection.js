e00951b749eb6f847e554da3d56cd7b01fb381ca
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-09-12 17:25:57 -0600

    updating dotenv file

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 //this is setting up our connection and creating a sequilize instance 
 //https://sequelize.org/v5/manual/getting-started.html
 //npm install --save sequelize
+//npm i sequelize
+//https://www.npmjs.com/package/sequelize
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-
+//https://devcenter.heroku.com/articles/jawsdb
+//"JawsDB is an add-on for providing a fully functional MySQL Database server for use with your Heroku application."
+//npm i dotenv
+//https://www.npmjs.com/package/dotenv
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
