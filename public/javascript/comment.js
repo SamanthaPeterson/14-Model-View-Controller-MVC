7c82946dc4fe81fc5648af191ccb54580892b28e
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-09-17 13:34:31 -0600

    ok

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,3 +1,6 @@
+//async function https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+//"async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding the need to explicitly configure promise chains." MDN Web Docs
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/async_function name, param, statement
 async function commentFormHandler(event) {
     event.preventDefault();
 
@@ -18,13 +21,15 @@ async function commentFormHandler(event) {
                 'Content-Type': 'application/json'
             }
         });
-
+//if else true false boolean "if true return... if false return..."
         if (response.ok) {
             document.location.reload();
         } else {
+            //https://developer.mozilla.org/en-US/docs/Web/API/Response/status
+            //status read-only property of the Response interface contains the status code of the response 
             alert(response.statusText);
         }
     }
 }
-
+//listen for the submit button 0
 document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
