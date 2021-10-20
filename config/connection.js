2fab3443998703defcdd0ea0cff8ad7ae1d939ba
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-10-20 16:12:26 -0600

    working on dotenv

--- config/connection.js
+++ config/connection.js
@@ -34,4 +34,17 @@ sequelize
     });
 
 
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_APP_DATABASE,
+        process.env.DB_USER,
+        process.env.DB_PASS, {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            dialect: 'mysql',
+        });
+}
+
 module.exports = sequelize;
