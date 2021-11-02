3ef9edeb1b88052a7ce3fe8d005829cdd5fed8cf
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-01 20:46:45 -0600

    update user

--- models/User.js
+++ models/User.js
@@ -1,7 +1,5 @@
-const {
-    Model,
-    DataTypes
-} = require('sequelize');
+const { Model, DataTypes } = 
+require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
