bf94ba053ff587cec56188e981682b5a57b3a17d
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-01 20:20:30 -0600

    update server

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 // Dependencies
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
-const exphbs = require('express-handlebars');
+
 const hbs = exphbs.create({
     helpers
 });
-const session = require('express-session');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
