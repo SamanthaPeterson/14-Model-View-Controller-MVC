e65e72e7df2ed9e9f0688dd15b4e0b68b02cb9e8
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-02 11:00:03 -0600

    error in server js

--- server.js
+++ server.js
@@ -3,16 +3,11 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-
+const routes = require('./controllers/');
+const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
-// const routes = require('./controllers');
-// const sequelize = require('./config/connection');
-// const helpers = require('./utils/helpers');
 
-// const hbs = exphbs.create({
-//     helpers
-// });
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -25,16 +20,12 @@ const sess = {
     saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize
-    //     checkExpirationInterval: 1000 * 60 * 10, // will check every 10 minutes
-    //     expiration: 1000 * 60 * 30 // will expire after 30 minutes
     })
 };
 
 const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({
-    helpers
-});
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
