51ebc8c9a6e65e7318fdd31a0e94c3b955bc2849
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-02 11:15:52 -0600

    still getting an error can't find module

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,12 +2,13 @@
 //Routing = how an application’s endpoints (URIs) respond to client requests
 //routing is defined using methods of the Express app
 const router = require('express').Router();
-
+const apiRoutes = require('/api');
 //nodejs is going to look for a routes.js file in your root path of your site
 //https://stackoverflow.com/questions/34247468/node-error-cannot-find-module-routes
 // routes are usually set up like this
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-routes = require('./routes')
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
 const commentRoutes = require('./comment-routes.js');
@@ -16,8 +17,22 @@ router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes)
 
-module.exports = router;
 
+//  path for the server for the API routes
+router.use('/api', apiRoutes);
+
+// path for the home page
+router.use('/', homeRoutes);
+
+//  path for the dashboard
+router.use('/dashboard', dashboardRoutes);
+
+//  catch-all route for any resource that doesn't exist
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
 //How to separate routes on Node.js and Express https://stackoverflow.com/questions/23923365/how-to-separate-routes-on-node-js-and-express-4
 //How to include route handlers in multiple files in Express https://stackoverflow.com/questions/6059246/how-to-include-route-handlers-in-multiple-files-in-express
 
