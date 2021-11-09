7c36f26b7f969e4cc86f4031f80f04186603a00b
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-08 23:18:06 -0700

    update help from tas

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -57,7 +57,8 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
-        password: req.body.password
+        password: req.body.password,
+        email: req.body.email
     })
         .then(dbUserData => {
             req.session.save(() => {
